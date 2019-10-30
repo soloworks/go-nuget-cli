abeef2b8a72fcac9fc8b0c2c2d8488bd9c9cb42b
Sam Shelton <sam.shelton@soloworks.co.uk>
2019-10-30 20:16:47 +0000

    Fix for globs

--- pack.go
+++ pack.go
@@ -14,13 +14,13 @@ import (
 	"github.com/urfave/cli"
 )
 
-func archiveFile(filename string, w *zip.Writer, b []byte) {
+func archiveFile(fn string, w *zip.Writer, b []byte) {
 
 	// Check and convert filepath to `/` if required
-	filename = filepath.ToSlash(filename)
+	fn = filepath.ToSlash(fn)
 
 	// Create the file in the zip
-	f, err := w.Create(filename)
+	f, err := w.Create(fn)
 	checkError(err)
 
 	// Write .nuspec bytes to file
@@ -31,16 +31,16 @@ func archiveFile(filename string, w *zip.Writer, b []byte) {
 // Package up a NuSpec file
 func packNupkg(c *cli.Context) error {
 
-	filename := c.Args().First()
+	nsfilename := c.Args().First()
 
 	// Check .nuspec file has been supplied
-	if filename == "" {
+	if nsfilename == "" {
 		return errors.New("Error NU5002: Please specify a nuspec file to use")
 	}
 	// Log out
-	fmt.Println("Attempting to build package from '" + filename + "'.")
+	fmt.Println("Attempting to build package from '" + nsfilename + "'.")
 	// Read in the nuspec file
-	n, err := nuspec.FromFile(filename)
+	n, err := nuspec.FromFile(nsfilename)
 	checkError(err)
 
 	// Override Version if option is set
@@ -49,7 +49,7 @@ func packNupkg(c *cli.Context) error {
 	}
 
 	// Set BasePath based on file provided
-	basePath := filepath.Dir(filename)
+	basePath := filepath.Dir(nsfilename)
 
 	// Override basePath if option is set
 	if bp := c.String("BasePath"); bp != "" {
@@ -82,23 +82,27 @@ func packNupkg(c *cli.Context) error {
 	// Add .nuspec to Archive
 	b, err := n.ToBytes()
 	checkError(err)
-	archiveFile(filepath.Base(filename), w, b)
-	ct.Add(filepath.Ext(filename))
+	archiveFile(filepath.Base(nsfilename), w, b)
+	ct.Add(filepath.Ext(nsfilename))
 
 	// Process files
 	// If there are no file globs specified then
 	if len(n.Files.File) == 0 {
 		// walk the basePath and zip up all found files. Everything.]
 		err = filepath.Walk(basePath, func(path string, info os.FileInfo, err error) error {
-			if !info.IsDir() && filepath.Base(path) != filepath.Base(filename) {
+			if !info.IsDir() && filepath.Base(path) != filepath.Base(nsfilename) {
+				// Open the file
 				x, err := os.Open(path)
 				checkError(err)
+				// Gather all contents
 				y, err := ioutil.ReadAll(x)
 				checkError(err)
+				// Set relative path for file in archive
 				p, err := filepath.Rel(basePath, path)
 				checkError(err)
+				// Store the file
 				archiveFile(p, w, y)
-
+				// Add extension to the Rels file
 				ct.Add(filepath.Ext(p))
 			}
 			return nil
@@ -107,18 +111,28 @@ func packNupkg(c *cli.Context) error {
 	} else {
 		// For each of the specified globs, get files an put in target
 		for _, f := range n.Files.File {
-			matches, err := filepath.Glob(f.Source)
+			// Apply glob, cater for
+			matches, err := filepath.Glob(filepath.ToSlash(filepath.Join(basePath, f.Source)))
 			checkError(err)
 			for _, m := range matches {
 				info, err := os.Stat(m)
-				if !info.IsDir() && filepath.Base(m) != filepath.Base(filename) {
+				if !info.IsDir() && filepath.Base(m) != filepath.Base(nsfilename) {
+					// Open the file
 					x, err := os.Open(m)
 					checkError(err)
+					// Gather all contents
 					y, err := ioutil.ReadAll(x)
 					checkError(err)
+					// Set relative path for file in archive
 					p, err := filepath.Rel(basePath, m)
 					checkError(err)
+					// Overide path if Target is set
+					if f.Target != "" {
+						p = filepath.Join(f.Target, filepath.Base(m))
+					}
+					// Store the file
 					archiveFile(p, w, y)
+					// Add extension to the Rels file
 					ct.Add(filepath.Ext(p))
 				}
 				checkError(err)
@@ -147,7 +161,7 @@ func packNupkg(c *cli.Context) error {
 
 	// Create and add .rels to Archive
 	rf := NewRelFile()
-	rf.Add("http://schemas.microsoft.com/packaging/2010/07/manifest", "/"+filepath.Base(filename))
+	rf.Add("http://schemas.microsoft.com/packaging/2010/07/manifest", "/"+filepath.Base(nsfilename))
 	rf.Add("http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties", pfn)
 
 	b, err = rf.ToBytes()
